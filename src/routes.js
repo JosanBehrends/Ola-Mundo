73f621e3dde83c4360ae4bf7da95419372f7e798
Josan Behrends <josanbehrends@gmail.com>
2024-01-09 16:35:47 -0300

    Início do Sobremim

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/sobremim";
 import Menu from "./componentes/menu";
+import Rodape from "./componentes/Rodape";
+import PaginaPadrao from "./componentes/PaginaPadrao";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
+
       <Routes>
-        <Route path="/" element={<Inicio />}/>
-        <Route path="/sobremim" element={<SobreMim />}/>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />}/>
+          <Route path="/sobremim" element={<SobreMim />}/>
+        </Route>
+        
         <Route path="*" element={<div>Página não encontrada</div>}/>
       </Routes>
+
+      <Rodape />
     </BrowserRouter>
   );
 }
