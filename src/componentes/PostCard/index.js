16a7d09dd1584fc71378d280a14b965050465edb
Josan Behrends <josanbehrends@gmail.com>
2024-01-10 13:44:58 -0300

    Início da alteração do Post

--- src/componentes/PostCard/index.js
+++ src/componentes/PostCard/index.js
@@ -1,16 +1,19 @@
+import { Link } from 'react-router-dom';
 import styles from './Post.module.css';
 
 export default function PostCard({post}) {
     return (
-        <div className={styles.post}>
-            <img 
-                className={styles.capa}
-                src={`assets/posts/${post.id}/capa.png`}
-                alt="Imagem de Capa do Post"
-            />
+        <Link to={`/posts/${post.id}`}>
+            <div className={styles.post}>
+                <img 
+                    className={styles.capa}
+                    src={`assets/posts/${post.id}/capa.png`}
+                    alt="Imagem de Capa do Post"
+                />
 
-            <h2 className={styles.titulo}>{post.titulo}</h2>
-            <button className={styles.botaoLer}>Ler</button>
-        </div>
+                <h2 className={styles.titulo}>{post.titulo}</h2>
+                <button className={styles.botaoLer}>Ler</button>
+            </div>
+        </Link>
     )
 }
